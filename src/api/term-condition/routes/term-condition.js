bdbe81358aa0a4d3e3439e745daefd2ff8d07b75
Young Gyu Park <ygpark2@gmail.com>
2024-03-18 15:24:35 +0900

    update schema

---
+++ src/api/term-condition/routes/term-condition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * faq router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::term-condition.term-condition');
---
+++ src/api/term-condition/services/term-condition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * faq service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::term-condition.term-condition');
